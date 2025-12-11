50ea31d7dbe53de8b2ba77897742a7c4dd3ea245
hendrisatya <nugraha.hendrisatya@protonmail.com>
2025-12-11 21:51:34 +0700

    fixed exception handler to use proper annotation

--- src/main/java/dev/nugraha/obstest/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/nugraha/obstest/exception/GlobalExceptionHandler.java
@@ -6,9 +6,9 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestController
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Map<String, Object>> handleRuntimeException(RuntimeException ex) {
